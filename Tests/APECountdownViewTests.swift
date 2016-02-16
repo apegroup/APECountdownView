f5de95882023ff9c9500cb1e2ffb0a1dc835ebf5
Daniel Nilsson <daniel.nilsson@apegroup.com>
2016-02-16 10:31:02 +0100

    Adds test case

--- Tests/APECountdownViewTests.swift
+++ Tests/APECountdownViewTests.swift
@@ -37,9 +37,62 @@ class APECountdownViewTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCompleteClosure() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+        let countdownView = CountdownView(frame: CGRect(x: 0, y: 0, width: 200, height: 400))
+        view.addSubview(countdownView)
+        
+        let asyncExpectation = expectationWithDescription("Complete closure expectation")
+        
+        let countdownTime = 3.0 // 3 sec
+        let endDate = NSDate(timeIntervalSinceNow: countdownTime)
+        countdownView.startCountdown(endDate, onCompleted: { _ in
+            asyncExpectation.fulfill()
+        })
+        
+        waitForExpectationsWithTimeout(countdownTime + 1, handler: { error in
+            XCTAssertNil(error, "Complete closure not called.")
+        })
+    }
+    
+    func testAppearance() {
+        let countdownView = CountdownView(frame: CGRect(x: 0, y: 0, width: 200, height: 400))
+        countdownView.size = CGSize(width: 40, height: 30)
+        countdownView.groupSpace = 3
+        countdownView.sectionSpace = 3
+        countdownView.gradientColor1 = UIColor.blackColor()
+        countdownView.gradientColor2 = UIColor.redColor()
+        countdownView.gradientColor3 = UIColor.greenColor()
+        countdownView.gradientColor4 = UIColor.blueColor()
+        countdownView.blockFontName = UIFont.familyNames().first!
+        countdownView.blockFontSize = 12
+        countdownView.blockFont = UIFont.systemFontOfSize(20)
+        countdownView.blockFontColor = UIColor.brownColor()
+        countdownView.titleFontName = UIFont.familyNames().last!
+        countdownView.titleFontSize = 8
+        countdownView.titleFontColor = UIColor.yellowColor()
+        countdownView.titleFont = UIFont.systemFontOfSize(20)
+    }
+    
+    func testStringExtensions() {
+        let testString1 = ""
+        XCTAssertEqual(testString1, testString1.getFirstChar())
+        XCTAssertEqual(testString1, testString1.getLastChar())
+        
+        let testString2 = String()
+        XCTAssertEqual(testString2, testString2.getFirstChar())
+        XCTAssertEqual(testString2, testString2.getLastChar())
+        
+        let testString3 = "asdf"
+        XCTAssertEqual("a", testString3.getFirstChar())
+        XCTAssertEqual("f", testString3.getLastChar())
+    }
+    
+    func testNumberView() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+        let countdownNumberView = CountdownNumberView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        countdownNumberView.drawRect(countdownNumberView.frame)
+        view.addSubview(countdownNumberView)
     }
     
     func testPerformanceExample() {
